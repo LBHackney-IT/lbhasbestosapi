826afb930629c1c1f61b7d257ce9755e70a2e133
Luke Morton <lukemorton.dev@gmail.com>
2018-04-23 11:40:21 +0100

    Use Hackney::AbsestosAPI::ViewPlan use case in PlansController#show

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,4 +1,11 @@
 class PlansController < ApplicationController
   def show
+    render json: view_plan_use_case.view
+  end
+
+  private
+
+  def view_plan_use_case
+    Hackney::AsbestosAPI::ViewPlan.new
   end
 end
