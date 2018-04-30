09c1673a562439896805cd335f5c274e88328416
Ferenc Schultesz <ferenc.schultesz@hackney.gov.uk>
2018-04-30 13:11:01 +0100

    Hackney::AbsestosAPI::ViewInspection use case in InspectionsController#show

--- app/controllers/inspections_controller.rb
+++ app/controllers/inspections_controller.rb
@@ -1,4 +1,13 @@
 class InspectionsController < ApplicationController
     def show
-    end
+    render json: view_inspection_use_case.view(params.fetch(:id))
+  end
+
+  private
+
+  def view_inspection_use_case
+    Hackney::AsbestosAPI::ViewInspection.new(
+      asbestos_gateway: Hackney::AsbestosAPI::DummyAsbestosGateway.new
+    )
+  end
 end
